9e11a5a9225de70b7b6cb8a89881930802e2e211
Rossi Kwak <rossi.kwak@gmail.com>
2021-06-21 12:02:45 +0900

    레지스터별 값 표시 UI요소 디자인

--- gZeroMaster/gZeroMaster/Resource.h
+++ gZeroMaster/gZeroMaster/Resource.h
@@ -10,6 +10,21 @@
 #define IDC_LOG_LIST                    1000
 #define IDC_COM_COMBO                   1001
 #define IDC_CONNECT_BUTTON              1002
+#define IDC_RX_REG1_STATIC              1003
+#define IDC_TX_REG1_TOP_STATIC          1004
+#define IDC_TX_REG1_MID_STATIC          1005
+#define IDC_TX_REG1_BOT_STATIC          1006
+#define IDC_TX_REG2_TOP_STATIC          1007
+#define IDC_TX_REG2_MID_STATIC          1008
+#define IDC_TX_REG2_BOT_STATIC          1009
+#define IDC_BIAS_REG1_STATIC            1010
+#define IDC_BIAS_REG2_STATIC            1011
+#define IDC_BIAS_REG3_STATIC            1012
+#define IDC_BIAS_REG4_STATIC            1013
+#define IDC_BIAS_REG5_STATIC            1014
+#define IDC_BIAS_REG6_STATIC            1015
+#define IDC_BIAS_REG7_STATIC            1016
+#define IDC_BIAS_REG8_STATIC            1017
 
 // Next default values for new objects
 // 
@@ -17,7 +32,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
