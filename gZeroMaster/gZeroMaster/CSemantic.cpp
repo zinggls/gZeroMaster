01aa66f8a9ca6904ba95edf0a28c85f4717aa01a
Rossi Kwak <rossi.kwak@gmail.com>
2021-07-06 14:35:25 +0900

    ReadAll시에 값 지우는 기능 적용

--- gZeroMaster/gZeroMaster/CSemantic.cpp
+++ gZeroMaster/gZeroMaster/CSemantic.cpp
@@ -886,6 +886,7 @@ CString CSemantic::DecToBin(int dec)
 void CSemantic::OnBnClickedReadAllButton()
 {
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	ResetValues();
 	Parent()->m_pRaw->OnBnClickedReadAllButton();
 	UpdateRegisters();
 }
