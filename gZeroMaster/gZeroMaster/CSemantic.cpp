6595740985705eaddde9addd76354e7fc598c0ed
Rossi Kwak <rossi.kwak@gmail.com>
2021-07-06 11:37:18 +0900

    ModPower선택시 컨트롤 콤보 박스 동작 구현

--- gZeroMaster/gZeroMaster/CSemantic.cpp
+++ gZeroMaster/gZeroMaster/CSemantic.cpp
@@ -983,6 +983,15 @@ void CSemantic::OnStnClickedModulatorPowerValueStatic()
 	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
 	m_selected = ModPow;
 	ShowSlider(SW_HIDE);
+
+	for (int i = m_controlCombo.GetCount() - 1; i >= 0; i--) m_controlCombo.DeleteString(i);
+	m_controlCombo.AddString(_T("Mod Off"));	//0
+	m_controlCombo.AddString(_T("Mod On"));		//1
+
+	CRegister reg;
+	UpdateTxReg2(Parent()->m_pRaw->m_strTxReg2Top, Parent()->m_pRaw->m_strTxReg2Mid, Parent()->m_pRaw->m_strTxReg2Bot, reg);
+	m_controlCombo.SetCurSel(reg.m_nModPower);
+
 	GetDlgItem(IDC_CONTROL_COMBO)->ShowWindow(SW_SHOW);
 	GetDlgItem(IDC_SELECTED_STATIC)->ShowWindow(SW_SHOW);
 	Invalidate();
