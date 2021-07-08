7f3447bb541e0910253ca7173837a4319ea4e9ae
Rossi Kwak <rossi.kwak@gmail.com>
2021-07-08 13:46:22 +0900

    0으로 TrimLeft적용

--- gZeroMaster/gZeroMaster/CSemantic.cpp
+++ gZeroMaster/gZeroMaster/CSemantic.cpp
@@ -1153,7 +1153,7 @@ int CSemantic::SliderValueUpdate()
 	m_strSliderValue.Format(_T("Dec:%d"), curPos);
 	m_strSliderValueHex.Format(_T("Hex:0x%02x"), curPos);
 	m_strSliderValueBin = _T("Bin:");
-	m_strSliderValueBin+=DecToBin(curPos).TrimLeft(_T('0')).TrimLeft();
+	m_strSliderValueBin+= (DecToBin(curPos).TrimLeft(_T('0')));
 	UpdateData(FALSE);
 	return curPos;
 }
