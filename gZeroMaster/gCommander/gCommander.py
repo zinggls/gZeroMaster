d80edab0a066feb042b49f15dba86e6827018b16
Rossi Kwak <rossi.kwak@gmail.com>
2021-11-03 17:33:35 +0900

    들여쓰기 수준 변경

--- gZeroMaster/gCommander/gCommander.py
+++ gZeroMaster/gCommander/gCommander.py
@@ -34,17 +34,17 @@ print("Response:%s" %socket.recv_string())
 #LNA Gain을 0에서 부터 10까지 변경 테스트
 #gZeroMaster에서 0~7까지는 범위내 값으로 받아들이고 8이후는 out of range 오류를 발생해야 함
 for i in range(10):
-        raw = r'{ "RX": {  "LNA Gain": '
-        raw += str(i)
-        raw += '} }'
+    raw = r'{ "RX": {  "LNA Gain": '
+    raw += str(i)
+    raw += '} }'
 
-        print("I=%s %s" %(i,raw))
-        socket.send_string(raw)
-        rtn = socket.recv_string()
-        print("\tResponse:%s" %rtn)
+    print("I=%s %s" %(i,raw))
+    socket.send_string(raw)
+    rtn = socket.recv_string()
+    print("\tResponse:%s" %rtn)
 
-        if rtn=="OK":   #성공하는 경우에만 json객체의 값을 업데이트 한다
-            json_data["RX"]["LNA Gain"]=i
-            print("\tjson_data updated")
+    if rtn=="OK":   #성공하는 경우에만 json객체의 값을 업데이트 한다
+        json_data["RX"]["LNA Gain"]=i
+        print("\tjson_data updated")
 
-        time.sleep(1)
+    time.sleep(1)
