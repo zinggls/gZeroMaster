afcabe945d09ba0913c0a4a13f4054c73b05d285
Rossi Kwak <rossi.kwak@gmail.com>
2021-11-03 14:40:28 +0900

    LNA Gain값을 0에서 9까지 변경하는 샘플 테스트

--- gZeroMaster/gCommander/gCommander.py
+++ gZeroMaster/gCommander/gCommander.py
@@ -1,4 +1,5 @@
 import zmq
+import time
 
 context = zmq.Context()
 
@@ -12,3 +13,14 @@ f.close()
 
 socket.send_string(data);
 print("Response:%s" %socket.recv_string())
+
+#LNA Gain을 0에서 부터 10까지 변경 테스트
+#gZeroMaster에서 0~7까지는 범위내 값으로 받아들이고 8이후는 out of range 오류를 발생해야 함
+for i in range(10):
+        raw = r'{ "RX": {  "LNA Gain": '
+        raw += str(i)
+        raw += '} }'
+
+        socket.send_string(raw)
+        print("Response:%s" %socket.recv_string())
+        time.sleep(1)
