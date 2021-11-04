79d6d66031b6ea4e39193d5af1484efa7255522a
Rossi Kwak <rossi.kwak@gmail.com>
2021-11-04 09:46:47 +0900

    데이터 타입 주석으로 표시

--- gZeroMaster/gCommander/gCommander.py
+++ gZeroMaster/gCommander/gCommander.py
@@ -7,11 +7,11 @@ socket = util.connect("tcp://localhost:5555")
 jsonFilePath = "..\\tests\\rx.json"
 print("Input json file=%s" %jsonFilePath)
 
-json_data = util.jsonLoad(jsonFilePath)
+json_data = util.jsonLoad(jsonFilePath)     #json_data type = <class 'dict'>
 print("json load OK")
 print(json_data)
 
-data = util.readData(jsonFilePath)
+data = util.readData(jsonFilePath)          #data type = <class 'str'>
 print(data)
 
 socket.send_string(data);
