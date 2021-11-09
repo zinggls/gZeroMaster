4889408176974096184cd5c66025688d8b34a4b7
Rossi Kwak <rossi.kwak@gmail.com>
2021-11-09 16:52:53 +0900

    idn함수 정의및 적용

--- gZeroMaster/commTest/test2.py
+++ gZeroMaster/commTest/test2.py
@@ -1,6 +1,9 @@
 import pyvisa
 import time
 
+def idn(inst):
+    print(inst.query("*IDN?"))                                      #N4903B 기기 정보 확인
+
 def reset():
     inst.write("*rst")                                              #N4903B 리셋
     print("N4903B 리셋")
@@ -32,8 +35,7 @@ rm=pyvisa.ResourceManager()
 print("resources = %s"%rm.list_resources())
 inst = rm.open_resource(rm.list_resources()[0])
 
-print(inst.query("*IDN?"))                                      #N4903B 기기 정보 확인
-
+idn(inst)
 reset()
 volt(inst)
 dataRate(inst)
