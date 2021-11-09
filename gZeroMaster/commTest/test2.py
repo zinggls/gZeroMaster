4ea99d1c29df3da8561862749d50ced2f9e9a40a
Rossi Kwak <rossi.kwak@gmail.com>
2021-11-09 16:23:43 +0900

    주석 탭 추가

--- gZeroMaster/commTest/test2.py
+++ gZeroMaster/commTest/test2.py
@@ -4,4 +4,4 @@ rm=pyvisa.ResourceManager()
 print("resources = %s"%rm.list_resources())
 inst = rm.open_resource(rm.list_resources()[0])
 
-print(inst.query("*IDN?"))  #N4903B 기기 정보 확인
+print(inst.query("*IDN?"))                                      #N4903B 기기 정보 확인
