a44ac39767134228466cbd879f3535cf3d5d762d
Rossi Kwak <rossi.kwak@gmail.com>
2021-11-09 16:11:40 +0900

    주석추가

--- gZeroMaster/commTest/test2.py
+++ gZeroMaster/commTest/test2.py
@@ -3,4 +3,5 @@ import pyvisa
 rm=pyvisa.ResourceManager()
 print("resources = %s"%rm.list_resources())
 inst = rm.open_resource(rm.list_resources()[0])
-print(inst.query("*IDN?"))
+
+print(inst.query("*IDN?"))  #N4903B 기기 정보 확인
