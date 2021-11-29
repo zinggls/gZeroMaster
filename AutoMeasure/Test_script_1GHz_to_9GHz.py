2b13d1806e8498ba4544ed5cdcc8aeebb08379b4
glskmc724 <kmc724@zinggls.com>
2021-11-29 12:47:20 +0900

    BERT의 Data Rate Frequency를 1GHz 에서 9GHz 까지 설정 할 수 있는 함수 set_frequency 작성
    또한 데이터 패턴을 설정할 수 있는 set_data_pattern 작성
    데이터 패턴은 PRBS, PRBN, ZSEQ, MDEN, UPAT 사용 가능
    파일을 사용한 데이터 패턴은 BERT PC에 파일이 저장되어 있어야 사용 가능
    
    두 함수 모두 예외처리 작성

--- AutoMeasure/Test_script_1GHz_to_9GHz.py
+++ AutoMeasure/Test_script_1GHz_to_9GHz.py
@@ -4,7 +4,54 @@ rm = pyvisa.ResourceManager()
 inst = rm.open_resource("TCPIP0::192.168.0.66::INSTR")
 manufacturer, product, serial_num, firmware_ver = inst.query("*IDN?").replace("\n", "").split(",")
 
+def set_frequency(ghz):
+    if (ghz >= 1 and ghz <= 9):
+        inst.write("source9:frequency {}e9".format(ghz))
+    else:
+        print("Not available frequency")
+
+def set_data_pattern(pattern): # Programming guide p105.
+    patt = pattern[0:4]
+    num = pattern[4:]
+    if (patt == "ZSUB" and pattern[4:9] == "stitut"):
+        if (num == "7" or num == "10" or num == "11" or num == "13" or num == "15" or num == "23"):
+            inst.write("source1:pattern:select {}".format(pattern))
+        else:
+            print("Not available data pattern")
+    elif (patt == "UPAT" and pattern[4:7] == "tern"):
+        if (int(num) >= 1 and int(num) <= 12):
+            inst.write("source1:pattern:select {}".format(pattern))
+        else:
+            print("Not available data pattern")
+    elif (patt == "MDEN" and pattern[4:7] == "sity"):
+        if (num == "7" or num == "10" or num == "11" or num == "13" or num == "15" or num == "23"):
+            inst.write("source1:pattern:select {}".format(pattern))
+        else:
+            print("Not available data pattern")
+    elif (patt == "PRBS" or patt == "PRBN" or patt == "MDEN" or patt == "ZSUB"):
+        if (num == "7" or num == "10" or num == "11" or num == "15" or num == "23"):
+            inst.write("source1:pattern:select {}".format(pattern))
+        elif ((num == "13" and (patt == "PRBN" or patt == "MDEN" or patt == "ZSUB")) \
+                 or (num == "31" and patt == "PRBS")):
+            inst.write("source1:pattern:select {}".format(pattern))
+        else:
+            print("Not available data pattern")
+    elif (patt == "UPAT"):
+        if (int(num) >= 1 and int(num) <= 12):
+            inst.write("source1:pattern:select {}".format(pattern))
+        else:
+            print("Not available data pattern")
+    elif (pattern[0:3] == "FIL" or pattern[0:8] == "FILename"):
+        inst.write("source1:pattern:select {}".format(pattern))
+    elif (pattern == "PRBS23P"):
+        inst.write("source1:pattern:select {}".format(pattern))
+    else:
+        print("Not available data pattern")
+
 if (product == "N4903B"):
     print("Found BERT")
+
+    set_frequency(10)
+    set_data_pattern("PRBS27")
 else:
     print("Not found BERT")
