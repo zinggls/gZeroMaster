869a9707c407519f70504961bd5ec54c6ed510a5
glskmc724 <kmc724@zinggls.com>
2021-12-08 14:51:35 +0900

    Testing_Main.py
    pyVISA_Lib와 ZeroMQ_Lib를 사용하여 gZeroMaster에서 B0칩의 레지스터를 JSON 형식으로 gZeroMaster_JSON.json 내용으로 변경하며, 변경이 완료된 이 후 BERT를 통해 3초간 전송속도를 변경하면서 실험을 하고 그에대한 결과를 result.csv 파일에 저장

---
+++ AutoMeasure/Test Scripts/Testing_Main.py
@@ -0,0 +1,39 @@
+import ZeroMQ_Lib
+import pyVISA_Lib
+import time
+
+"""
+gZero_TX = ZeroMQ_Lib.gZeroMaster_Lib(5555)
+gZero_RX = ZeroMQ_Lib.gZeroMaster_Lib(5556)
+
+gZero_TX.json_file_load("gZeroMaster_JSON.json")
+gZero_RX.json_file_load("gZeroMaster_JSON.json")
+
+gZeroTX.send_all_command()
+gZeroRX.send_all_command()
+"""
+
+"""
+Atten = ZeroMQ_Lib.Attenuator_Lib(1234)
+Atten.set_atten(4)
+"""
+
+pyVISA = pyVISA_Lib.pyVISA_Lib()
+inst = pyVISA.get_inst()
+csv = open("result.csv", "w")
+csv.write("Freq (GHz),Bit count,Error count,Bit Error Rate\n")
+
+for ghz in range(1, 10):
+    time.sleep(1)
+    if (pyVISA.get_opc() == True):
+        pyVISA.set_frequency(ghz)
+        ret = pyVISA.auto_align()
+        if (ret == True):
+            bcount, ecount, eratio = pyVISA.accumulate()
+        
+            csv_data = "{},{},{},{}\n".format(ghz, bcount, ecount, eratio)
+        else:
+            csv_data = "{},{},{},{}\n".format(ghz, "ALIGN FAILED", 0, 0)
+        csv.write(csv_data)
+        
+csv.close()
