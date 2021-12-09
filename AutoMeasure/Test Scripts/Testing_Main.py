91f49fd81728e1abc0ad21760e365cc7346697b3
glskmc724 <kmc724@zinggls.com>
2021-12-09 09:54:32 +0900

    오타 수정

--- AutoMeasure/Test Scripts/Testing_Main.py
+++ AutoMeasure/Test Scripts/Testing_Main.py
@@ -2,21 +2,17 @@ import ZeroMQ_Lib
 import pyVISA_Lib
 import time
 
-"""
 gZero_TX = ZeroMQ_Lib.gZeroMaster_Lib(5555)
 gZero_RX = ZeroMQ_Lib.gZeroMaster_Lib(5556)
 
 gZero_TX.json_file_load("gZeroMaster_JSON.json")
 gZero_RX.json_file_load("gZeroMaster_JSON.json")
 
-gZeroTX.send_all_command()
-gZeroRX.send_all_command()
-"""
+gZero_TX.send_all_command()
+gZero_RX.send_all_command()
 
-"""
-Atten = ZeroMQ_Lib.Attenuator_Lib(1234)
-Atten.set_atten(4)
-"""
+#Atten = ZeroMQ_Lib.Attenuator_Lib(1234)
+#Atten.set_atten(4)
 
 pyVISA = pyVISA_Lib.pyVISA_Lib()
 inst = pyVISA.get_inst()
@@ -30,7 +26,7 @@ for ghz in range(1, 10):
         ret = pyVISA.auto_align()
         if (ret == True):
             bcount, ecount, eratio = pyVISA.accumulate()
-        
+
             csv_data = "{},{},{},{}\n".format(ghz, bcount, ecount, eratio)
         else:
             csv_data = "{},{},{},{}\n".format(ghz, "ALIGN FAILED", 0, 0)
