ec6c781edf6e2161773bd2d556b13975d27550ff
glskmc724 <kmc724@zinggls.com>
2021-12-09 09:53:55 +0900

    get_query 함수 명을 query로 변경
    auto_align 함수의 while 루프를 for 루프로 변경

--- AutoMeasure/Test Scripts/pyVISA_Lib.py
+++ AutoMeasure/Test Scripts/pyVISA_Lib.py
@@ -12,46 +12,38 @@ class pyVISA_Lib:
     def get_idc(self):
         return self.inst.query("*IDN?")
 
-    def get_query(self, query_str):
+    def query(self, query_str):
         return self.inst.query(query_str).replace("\n", "")
 
     def write(self, write_str):
         self.inst.write(write_str)
 
     def get_opc(self):
-        return bool(self.get_query("*OPC?"))
+        return bool(self.query("*OPC?"))
     
     def set_pattern(self, pattern):
         self.write("source9:pattern:select {}".format("PRBS7"))
-        #self.inst.write("source9:pattern:select {}".format(pattern))
+        #self.write("source9:pattern:select {}".format(pattern))
 
     def set_frequency(self, ghz):
         self.write("source9:frequency {}e9".format(ghz))
 
     def auto_align(self):
-        flag = 1
-        count = 0
-        while flag == 1:
+        for cnt in range(5):
             self.write("sense1:eye:align:auto on")
             while True:
                 time.sleep(1)
-                state = self.get_query("sense1:eye:align:auto?")
+                state = self.query("sense1:eye:align:auto?")
                 if (state == "CS_SUCCESSFUL"):
-                    flag = 0
                     print("Align successful")
-                    ret = True
-                    break
+                    return True
                 elif (state == "CS_FAILED"):
                     print("Align failed")
                     self.write("sense1:eye:align:auto 0")
-                    print("{}".format(self.get_query("sense1:eye:align:message?")))
-                    count += 1
-                    if (count == 5):
-                        print("Failed 5th time, align failed")
-                        flag = 0
-                        ret = False
+                    print("{}".format(self.query("sense1:eye:align:message?")))
                     break
-        return ret
+        print("Failed {}th time, align failed".format(cnt + 1))
+        return False
 
     def accumulate(self):
         self.write("sense:gate:mode SING")
@@ -61,12 +53,12 @@ class pyVISA_Lib:
 
         while True:
             time.sleep(1)
-            state = self.get_query("sense1:gate:state?")
+            state = self.query("sense1:gate:state?")
             if (state == "0"):
                 break
 
-        bcount = int(float(self.get_query("fetch:sense2:bcount?")))
-        ecount = int(float(self.get_query("fetch:sense1:ecount:all:full:total?")))
-        eratio = float(self.get_query("fetch:sense1:eratio:all:full:total?"))
+        bcount = int(float(self.query("fetch:sense2:bcount?")))
+        ecount = int(float(self.query("fetch:sense1:ecount:all:full:total?")))
+        eratio = float(self.query("fetch:sense1:eratio:all:full:total?"))
 
         return bcount, ecount, eratio
