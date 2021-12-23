7e7755b3c764a76e5d421fc64e98b1051153b91e
glskmc724 <kmc724@zinggls.com>
2021-12-23 09:45:14 +0900

    실험에 필요한 각종 상수를 변수로 변경
    gZeroMaster 클래스를 사용하여 2개의 객체를 다루도록 변경 및 실험 실패시 중단하도록 변경

--- AutoMeasure/Test Scripts/Testing_Main.py
+++ AutoMeasure/Test Scripts/Testing_Main.py
@@ -2,48 +2,50 @@ import ZeroMQ_Lib
 import pyVISA_Lib
 import time
 
-gZero_TX = ZeroMQ_Lib.gZeroMaster_Lib(5555)
-gZero_RX = ZeroMQ_Lib.gZeroMaster_Lib(5556)
+MIN_GHZ = 1
+MAX_GHZ = 7
+TRY_ALIGN = 1
+ACCUMULATE_TIME = 3
 
-gZero_TX.set_register("BIAS", "Core Current", 10)
-gZero_RX.set_register("BIAS", "Core Current", 10)
+gZeroMaster = ZeroMQ_Lib.gZeroMaster(5555, 5556)
+TX_Mod = gZeroMaster.gZero_TX
+RX_Mod = gZeroMaster.gZero_RX
 
-#gZero_TX.send_all_command()
-#gZero_RX.send_all_command()
+TX_Mod.set_register("TX", "PA Gain Control1", 0x0)
+TX_Mod.set_register("TX", "PA Gain Control2", 0x0)
+TX_Mod.set_register("TX", "VCO Oscillation Freq", 0x1)
 
-gZero_TX.send_command()
-gZero_RX.send_command()
+gZeroMaster.send_all()
+gZeroMaster.save_all()
+gZeroMaster.close_csv()
 
-Atten = ZeroMQ_Lib.Attenuator_Lib(12345)
-atten_list = [20]
+#Atten = ZeroMQ_Lib.Attenuator_Lib(12345)
+#atten_list = [22, 20, 18, 14, 12, 10]
+atten_list = [22]
 
-gZero_TX_reg_csv = open("reg_TX.csv", "w")
-gZero_RX_reg_csv = open("reg_RX.csv", "w")
 csv = open("result.csv", "w")
-gZero_TX.save_all_in_csv(gZero_TX_reg_csv, 5555)
-gZero_RX.save_all_in_csv(gZero_RX_reg_csv, 5556)
 for Atten_dB in atten_list:
 
-    Atten.set_atten(Atten_dB)
+    #Atten.set_atten(Atten_dB)
 
     pyVISA = pyVISA_Lib.pyVISA_Lib()
     inst = pyVISA.get_inst()
     csv.write("Freq (GHz) (ATT: {}dB),Bit count,Error count,Bit Error Rate\n".format(Atten_dB))
 
-    for ghz in range(1, 10):
+    for ghz in range(MIN_GHZ, MAX_GHZ + 1):
         time.sleep(1)
         if (pyVISA.get_opc() == True):
             pyVISA.set_frequency(ghz)
             time.sleep(2)
-            ret = pyVISA.auto_align(1)
-            if (ret == True):
-                bcount, ecount, eratio = pyVISA.accumulate(3)
+            ret = pyVISA.auto_align(TRY_ALIGN)
+            if (ret == True): 
+                bcount, ecount, eratio = pyVISA.accumulate(ACCUMULATE_TIME)
                 #print("{} GHz".format(ghz))
                 csv_data = "{},{},{},{}\n".format(ghz, bcount, ecount, eratio)
+                csv.write(csv_data)
             else:
-                csv_data = "{},{},{},{}\n".format(ghz, "ALIGN FAILED", 0, 0)
-            csv.write(csv_data)
+                csv_data = "{},{},{},{}\n".format(ghz, "ALIGN FAILED", "ERROR", "ERROR")
+                csv.write(csv_data)
+                break
         
 csv.close()
-gZero_TX_reg_csv.close()
-gZero_RX_reg_csv.close()
