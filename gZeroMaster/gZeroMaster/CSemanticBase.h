1b9d8fb6493e1a2faa6e83ba4b0e38d8900e1160
Rossi Kwak <rossi.kwak@gmail.com>
2024-01-29 16:30:51 +0900

    CRegister를 상위 클래스에서 선언하고 Parse관련 모든 static멤버 함수들을 상위 클래스로 이동

--- gZeroMaster/gZeroMaster/CSemanticBase.h
+++ gZeroMaster/gZeroMaster/CSemanticBase.h
@@ -1,6 +1,48 @@
 #pragma once
 
 class CgZeroMasterDlg;
+class CRaw;
+
+class CRegister {
+public:
+	//RX
+	int m_nRxData;
+	int m_nLimitAmp;
+	int m_nLnaGain;
+
+	//TX
+	int m_nDutyCycle;
+	int m_nVcoOsc;;
+	int m_nRegRef;
+	int m_nVcoVdd;
+	int m_nVcoPower;
+	int m_nModPower;
+	int m_nTestBufPower;
+	int m_nDataInpSel;
+	int m_nPaPower;
+	int m_nPaGainCon1;
+	int m_nPaGainCon2;
+	int m_nTestBufCur;
+
+	//BIAS
+	int m_nBiasBlock;
+	int m_nLna1Cur;
+	int m_nLna2Cur;
+	int m_nLna3Cur;
+	int m_nLna4Cur;
+	int m_nLna5Cur;
+	int m_nDemodRefStageCur;
+	int m_nDemodIPStageCur;
+	int m_nLaFBCur;
+	int m_nLaCoreCur;
+	int m_nLaOPBufCur;
+	int m_nLaIPBufCur;
+	int m_nLaHLDataRateCur;
+	int m_nCMOSGainStageCur;
+	int m_nCMLInterfaceStageCur;
+	int m_nFdCoreCur;
+	int m_nFdBufCur;
+};
 
 class CSemanticBase : public CDialogEx
 {
@@ -31,4 +73,17 @@ public:
 
 public:
 	CgZeroMasterDlg* Parent();
+	static void Parse(CRaw* pRaw, CRegister& reg);
+	static void UpdateRxReg1(CString strRxReg1, CRegister& reg);
+	static void UpdateTxReg1(CString strTxRegTop, CString strTxRegMid, CString strTxRegBot, CRegister& reg, CString chip);
+	static void UpdateTxReg2(CString strTxRegTop, CString strTxRegMid, CString strTxRegBot, CRegister& reg);
+	static void UpdateBiasReg1(CString strBiasReg1, CRegister& reg);
+	static void UpdateBiasReg2(CString strBiasReg2, CRegister& reg);
+	static void UpdateBiasReg3(CString strBiasReg3, CRegister& reg);
+	static void UpdateBiasReg4(CString strBiasReg4, CRegister& reg);
+	static void UpdateBiasReg5(CString strBiasReg5, CRegister& reg);
+	static void UpdateBiasReg6(CString strBiasReg6, CRegister& reg);
+	static void UpdateBiasReg7(CString strBiasReg7, CRegister& reg);
+	static void UpdateBiasReg8(CString strBiasReg8, CRegister& reg);
+	static void UpdateBiasReg9(CString strBiasReg9, CRegister& reg);
 };
