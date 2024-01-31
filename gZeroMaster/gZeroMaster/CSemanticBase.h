0aba01e99e215d367c1e092d48b27794b4a53ecb
Rossi Kwak <rossi.kwak@gmail.com>
2024-01-31 13:49:54 +0900

    SelectStatic을 전역 스코프에서 선언

--- gZeroMaster/gZeroMaster/CSemanticBase.h
+++ gZeroMaster/gZeroMaster/CSemanticBase.h
@@ -44,6 +44,11 @@ public:
 	int m_nFdBufCur;
 };
 
+enum class SelectStatic {
+	None = 0, RxData, LimAmp, LnaGain, DutyCycle, VcoOsc, RegRef, VcoVdd, VcoPow, ModPow, TestBufPow,
+	DataInp, PaPow, PaGain1, PaGain2, TestBuffer, BiasBlock, Lna1, Lna2, Lna3, Lna4, Lna5, RefStage, IpStage, Fb, Core, OP, IP, HL, CMOS, CML, FdCore, FdBuf
+};
+
 class CSemanticBase : public CDialogEx
 {
 	DECLARE_DYNAMIC(CSemanticBase)
@@ -56,11 +61,6 @@ public:
 	CSliderCtrl m_controlSlider;
 	BOOL m_bSemanticEdit;
 	BOOL m_bAutoWrite;
-
-	enum class SelectStatic {
-		None = 0, RxData, LimAmp, LnaGain, DutyCycle, VcoOsc, RegRef, VcoVdd, VcoPow, ModPow, TestBufPow,
-		DataInp, PaPow, PaGain1, PaGain2, TestBuffer, BiasBlock, Lna1, Lna2, Lna3, Lna4, Lna5, RefStage, IpStage, Fb, Core, OP, IP, HL, CMOS, CML, FdCore, FdBuf
-	};
 	SelectStatic m_selected;
 
 	CString m_strSelectedStatic;
