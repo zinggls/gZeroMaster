8ecc81e658a5153b09000709570cbede1ffd465c
Rossi Kwak <rossi.kwak@gmail.com>
2024-02-06 10:26:59 +0900

    코드 구조 개선을 위한 사전 작업으로 CRegister클래스를 별도의 파일로 추출하여 사용

--- gZeroMaster/gZeroMaster/CSemanticBase.h
+++ gZeroMaster/gZeroMaster/CSemanticBase.h
@@ -2,50 +2,10 @@
 
 #include <map>
 #include "SemanticUserDefinedMessage.h"
+#include "CRegister.h"
 
 class CgZeroMasterDlg;
 class CRaw;
-
-class CRegister {
-public:
-	//RX
-	int m_nRxData;
-	int m_nLimitAmp;
-	int m_nLnaGain;
-
-	//TX
-	int m_nDutyCycle;
-	int m_nVcoOsc;;
-	int m_nRegRef;
-	int m_nVcoVdd;
-	int m_nVcoPower;
-	int m_nModPower;
-	int m_nTestBufPower;
-	int m_nDataInpSel;
-	int m_nPaPower;
-	int m_nPaGainCon1;
-	int m_nPaGainCon2;
-	int m_nTestBufCur;
-
-	//BIAS
-	int m_nBiasBlock;
-	int m_nLna1Cur;
-	int m_nLna2Cur;
-	int m_nLna3Cur;
-	int m_nLna4Cur;
-	int m_nLna5Cur;
-	int m_nDemodRefStageCur;
-	int m_nDemodIPStageCur;
-	int m_nLaFBCur;
-	int m_nLaCoreCur;
-	int m_nLaOPBufCur;
-	int m_nLaIPBufCur;
-	int m_nLaHLDataRateCur;
-	int m_nCMOSGainStageCur;
-	int m_nCMLInterfaceStageCur;
-	int m_nFdCoreCur;
-	int m_nFdBufCur;
-};
 
 enum class SelectStatic {
 	None = 0, RxData, LimAmp, LnaGain, DutyCycle, VcoOsc, RegRef, VcoVdd, VcoPow, ModPow, TestBufPow,
