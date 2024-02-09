79c69f936da3b6aab0cfe39befbea64c2d2e105e
Rossi Kwak <rossi.kwak@gmail.com>
2024-02-09 13:12:33 +0900

    CRawZing400T 객체를 위해 regMap자료구조 값 초기화

--- gZeroMaster/gZeroMaster/CRawZing400T.cpp
+++ gZeroMaster/gZeroMaster/CRawZing400T.cpp
@@ -2,6 +2,7 @@
 //
 
 #include "pch.h"
+#include "gZeroMaster.h"
 #include "CRawZing400T.h"
 
 
@@ -50,6 +51,12 @@ void CRawZing400T::OnChipConnect(CString chipModel)
 	ASSERT(chipModel == _T("Zing400T"));
 
 	m_regMap.clear();
+	m_regMap.insert(std::pair<CString, CReg>(_T("TX_REG1 [23:16]"), CReg(7, &m_strTxReg1Top, IDC_TX_REG1_TOP_STATIC)));
+	m_regMap.insert(std::pair<CString, CReg>(_T("TX_REG1 [15:8]"), CReg(6, &m_strTxReg1Mid, IDC_TX_REG1_MID_STATIC)));
+	m_regMap.insert(std::pair<CString, CReg>(_T("TX_REG1 [7:0]"), CReg(5, &m_strTxReg1Bot, IDC_TX_REG1_BOT_STATIC)));
+	m_regMap.insert(std::pair<CString, CReg>(_T("TX_REG2 [16]"), CReg(13, &m_strTxReg2Top, IDC_TX_REG2_TOP_STATIC)));
+	m_regMap.insert(std::pair<CString, CReg>(_T("TX_REG2 [15:13]"), CReg(12, &m_strTxReg2Mid, IDC_TX_REG2_MID_STATIC)));
+	m_regMap.insert(std::pair<CString, CReg>(_T("TX_REG2 [3:0]"), CReg(11, &m_strTxReg2Bot, IDC_TX_REG2_BOT_STATIC)));
 }
 
 void CRawZing400T::OnBnClickedDefaultValueButton()
