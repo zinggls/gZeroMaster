0867c13a3690dfaeb3c424923e40e6a164e942f8
Rossi Kwak <rossi.kwak@gmail.com>
2024-02-09 12:27:59 +0900

    IDD_SEMANTIC_ZING400T_DIALOG에 정의된 GUI 요소들과 호환되도록 DDX구현

--- gZeroMaster/gZeroMaster/CSemanticZing400T.cpp
+++ gZeroMaster/gZeroMaster/CSemanticZing400T.cpp
@@ -22,6 +22,29 @@ CSemanticZing400T::~CSemanticZing400T()
 void CSemanticZing400T::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_DUTY_CYCLE_VALUE_STATIC, m_strDutyCycle);
+	DDX_Text(pDX, IDC_VCO_OSC_FREQ_VALUE_STATIC, m_strVcoOscFreq);
+	DDX_Text(pDX, IDC_VCO_OSC_FREQ_VALUE_STATIC, m_strVcoOscFreq);
+	DDX_Text(pDX, IDC_REG_REF_VOLT_VALUE_STATIC, m_strRegRefVolt);
+	DDX_Text(pDX, IDC_VCO_VDD_VALUE_STATIC, m_strVcoVdd);
+	DDX_Text(pDX, IDC_VCO_POWER_VALUE_STATIC, m_strVcoPower);
+	DDX_Text(pDX, IDC_MODULATOR_POWER_VALUE_STATIC, m_strModPower);
+	DDX_Text(pDX, IDC_TEST_BUFFER_POWER_VALUE_STATIC, m_strTestBufferPower);
+	DDX_Text(pDX, IDC_DATA_INPUT_SELECT_VALUE_STATIC, m_strDataInputSelect);
+	DDX_Text(pDX, IDC_PA_POWER_VALUE_STATIC, m_strPaPower);
+	DDX_Text(pDX, IDC_PA_GAIN_CONTROL1_VALUE_STATIC, m_strPaGainControl1);
+	DDX_Text(pDX, IDC_PA_GAIN_CONTROL2_VALUE_STATIC, m_strPaGainControl2);
+	DDX_Text(pDX, IDC_TEST_BUFFER_CURRENT_VALUE_STATIC, m_strTestBufferCurrent);
+	DDX_Control(pDX, IDC_CONTROL_SLIDER, m_controlSlider);
+	DDX_Check(pDX, IDC_SEMANTIC_EDIT_CHECK, m_bSemanticEdit);
+	DDX_Text(pDX, IDC_SELECTED_STATIC, m_strSelectedStatic);
+	DDX_Text(pDX, IDC_SLIDER_MIN_STATIC, m_strSliderMin);
+	DDX_Text(pDX, IDC_SLIDER_MAX_STATIC, m_strSliderMax);
+	DDX_Text(pDX, IDC_SLIDER_VALUE_STATIC, m_strSliderValue);
+	DDX_Text(pDX, IDC_SLIDER_VALUE_HEX_STATIC, m_strSliderValueHex);
+	DDX_Text(pDX, IDC_SLIDER_VALUE_BIN_STATIC, m_strSliderValueBin);
+	DDX_Control(pDX, IDC_CONTROL_COMBO, m_controlCombo);
+	DDX_Check(pDX, IDC_AUTO_WRITE_CHECK, m_bAutoWrite);
 }
 
 
