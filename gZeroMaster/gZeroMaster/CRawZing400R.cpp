259cb36a2b2162b2df8b27a9534406c96a397aef
Rossi Kwak <rossi.kwak@gmail.com>
2024-02-10 22:24:06 +0900

    CRaw의 OnInitDialog 처리과정을 동일하게 적용

--- gZeroMaster/gZeroMaster/CRawZing400R.cpp
+++ gZeroMaster/gZeroMaster/CRawZing400R.cpp
@@ -55,7 +55,12 @@ BOOL CRawZing400R::OnInitDialog()
 	CDialogEx::OnInitDialog();
 
 	// TODO:  여기에 추가 초기화 작업을 추가합니다.
+	BitControlEnable(FALSE);
+	GetDlgItem(IDC_EDIT_CHECK)->ShowWindow(SW_HIDE);
+	ShowWriteButtons(SW_HIDE);
+	GetDlgItem(IDC_READ_ALL_BUTTON)->ShowWindow(SW_HIDE);
 
+	ShowBitWindow(SW_HIDE);
 	return TRUE;  // return TRUE unless you set the focus to a control
 				  // 예외: OCX 속성 페이지는 FALSE를 반환해야 합니다.
 }
