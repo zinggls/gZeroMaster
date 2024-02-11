97f214e9eebe4d255b6ef0f4e548fbf8bad2bf0c
Rossi Kwak <rossi.kwak@gmail.com>
2024-02-11 16:02:38 +0900

    Zing400R Semantic을 위한 DDX구현

--- gZeroMaster/gZeroMaster/CSemanticZing400R.cpp
+++ gZeroMaster/gZeroMaster/CSemanticZing400R.cpp
@@ -23,6 +23,28 @@ CSemanticZing400R::~CSemanticZing400R()
 void CSemanticZing400R::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_RX_DATA_IF_ENABLE_VALUE_STATIC, m_strRxDataInterface);
+	DDX_Text(pDX, IDC_LIMITING_AMP_ENABLE_VALUE_STATIC, m_strLimitingAmplifier);
+	DDX_Text(pDX, IDC_BIAS_BLOCK_ENABLE_VALUE_STATIC, m_strBiasBlockEnable);
+	DDX_Text(pDX, IDC_DEMOD_REF_STAGE_CURRENT_VALUE_STATIC, m_strDemodRefStageCurrent);
+	DDX_Text(pDX, IDC_DEMOD_IP_STAGE_CURRENT_VALUE_STATIC, m_strDemodIPStageCurrent);
+	DDX_Text(pDX, IDC_LA_FB_CURRENT_VALUE_STATIC, m_strLaFBCurrent);
+	DDX_Text(pDX, IDC_LA_CORE_CURRENT_VALUE_STATIC, m_strLaCoreCurrent);
+	DDX_Text(pDX, IDC_LA_OP_BUFFER_CURRENT_VALUE_STATIC, m_strLaOPBufferCurrent);
+	DDX_Text(pDX, IDC_LA_IP_BUFFER_CURRENT_VALUE_STATIC, m_strLaIPBufferCurrent);
+	DDX_Text(pDX, IDC_LA_HL_DATA_RATE_CURRENT_VALUE_STATIC, m_strLaHLDataRateCurrent);
+	DDX_Text(pDX, IDC_CMOS_GAIN_STAGE_CURRENT_VALUE_STATIC, m_strCMOSGainStageCurrent);
+	DDX_Text(pDX, IDC_CML_INTERFACE_STAGE_CURRENT_VALUE_STATIC, m_strCMLInterfaceStageCurrent);
+	DDX_Control(pDX, IDC_CONTROL_SLIDER, m_controlSlider);
+	DDX_Check(pDX, IDC_SEMANTIC_EDIT_CHECK, m_bSemanticEdit);
+	DDX_Text(pDX, IDC_SELECTED_STATIC, m_strSelectedStatic);
+	DDX_Text(pDX, IDC_SLIDER_MIN_STATIC, m_strSliderMin);
+	DDX_Text(pDX, IDC_SLIDER_MAX_STATIC, m_strSliderMax);
+	DDX_Text(pDX, IDC_SLIDER_VALUE_STATIC, m_strSliderValue);
+	DDX_Text(pDX, IDC_SLIDER_VALUE_HEX_STATIC, m_strSliderValueHex);
+	DDX_Text(pDX, IDC_SLIDER_VALUE_BIN_STATIC, m_strSliderValueBin);
+	DDX_Control(pDX, IDC_CONTROL_COMBO, m_controlCombo);
+	DDX_Check(pDX, IDC_AUTO_WRITE_CHECK, m_bAutoWrite);
 }
 
 
