eb3058379772dd5ad8b3d4b11d47dba97e3dfd4b
Rossi Kwak <rossi.kwak@gmail.com>
2024-02-20 16:58:20 +0900

    CRegister에서 상속받은 CRegisterZing400T클래스 선언

--- gZeroMaster/gZeroMaster/CRegisterZing400T.h
+++ gZeroMaster/gZeroMaster/CRegisterZing400T.h
@@ -2,6 +2,16 @@
 
 #include "CRegister.h"
 
-class CRegisterZing400T : public CRegister {
+class CBfBlock {
+	int m_nBlock;
+	int m_nQ;
+	int m_nI;
+};
 
+class CRegisterZing400T : public CRegister {
+public:
+	int m_nVspsPa;
+	int m_nVspsVga;
+	int m_nVspsCs;
+	CBfBlock m_block[4];
 };
