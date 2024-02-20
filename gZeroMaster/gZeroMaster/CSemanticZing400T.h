881a60ef61ce6ff26ffb72d7e554890abea35c87
Rossi Kwak <rossi.kwak@gmail.com>
2024-02-20 20:54:48 +0900

    CSelect를 상위 클래스의 CSelect에서 상속받음

--- gZeroMaster/gZeroMaster/CSemanticZing400T.h
+++ gZeroMaster/gZeroMaster/CSemanticZing400T.h
@@ -13,6 +13,25 @@ public:
 	CSemanticZing400T(CWnd* pParent, CRawBase* pRawBase);   // 표준 생성자입니다.
 	virtual ~CSemanticZing400T();
 
+	class CSelect :public CSemanticBase::CSelect {
+	public:
+		static const UINT VspsPa = 33;
+		static const UINT VspsVga = 34;
+		static const UINT VspsCs = 35;
+		static const UINT Ch3Block = 36;
+		static const UINT Ch3_Q = 37;
+		static const UINT Ch3_I = 38;
+		static const UINT Ch2Block = 39;
+		static const UINT Ch2_Q = 40;
+		static const UINT Ch2_I = 41;
+		static const UINT Ch1Block = 42;
+		static const UINT Ch1_Q = 43;
+		static const UINT Ch1_I = 44;
+		static const UINT Ch0Block = 45;
+		static const UINT Ch0_Q = 46;
+		static const UINT Ch0_I = 47;
+	};
+
 	// 대화 상자 데이터입니다.
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_SEMANTIC_ZING400T_DIALOG };
