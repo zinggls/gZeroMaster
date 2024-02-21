c4ef2115c335e5995934cf532729696b2485a0bb
Rossi Kwak <rossi.kwak@gmail.com>
2024-02-22 01:51:47 +0900

    멤버 변수 정의

--- gZeroMaster/gZeroMaster/CRegisterZing400R.h
+++ gZeroMaster/gZeroMaster/CRegisterZing400R.h
@@ -1,8 +1,13 @@
 #pragma once
 
 #include "CRegister.h"
+#include "CBfBlock.h"
 
 class CRegisterZing400R : public CRegister {
 public:
 	virtual ~CRegisterZing400R() {}
+
+	CBfBlock m_block[4];
+	int m_nVspsBias;
+	int m_nLnaBias;
 };
