e28246bc9434d276de3e1a2a8e6bfceb6fa6c532
Rossi Kwak <rossi.kwak@gmail.com>
2024-02-24 19:58:47 +0900

    I와 Q에 관한 상수 정의 삭제후 Phase 관련 상수 추가

--- gZeroMaster/gZeroMaster/CSemanticZing400T.h
+++ gZeroMaster/gZeroMaster/CSemanticZing400T.h
@@ -21,17 +21,13 @@ public:
 		static const UINT VspsVga = 34;
 		static const UINT VspsCs = 35;
 		static const UINT Ch3Block = 36;
-		static const UINT Ch3_Q = 37;
-		static const UINT Ch3_I = 38;
-		static const UINT Ch2Block = 39;
-		static const UINT Ch2_Q = 40;
-		static const UINT Ch2_I = 41;
-		static const UINT Ch1Block = 42;
-		static const UINT Ch1_Q = 43;
-		static const UINT Ch1_I = 44;
-		static const UINT Ch0Block = 45;
-		static const UINT Ch0_Q = 46;
-		static const UINT Ch0_I = 47;
+		static const UINT Ch3Phase = 37;
+		static const UINT Ch2Block = 38;
+		static const UINT Ch2Phase = 39;
+		static const UINT Ch1Block = 40;
+		static const UINT Ch1Phase = 41;
+		static const UINT Ch0Block = 42;
+		static const UINT Ch0Phase = 43;
 	};
 
 	// 대화 상자 데이터입니다.
